663e4a68847ab2a1a79aba1802d5c08ded166ecb
itsKapri <nk92042@gmail.com>
2023-07-16 19:20:37 +0530

    changes in school route

--- routes/schoolRoutes.js
+++ routes/schoolRoutes.js
@@ -7,6 +7,6 @@ const authMiddleware = require('../middleware/auth');
 router.route("/school").get(getAllSchools)
 router.route("/school/new").post(createSchoolList)
 router.route("/school/:id").put(updateSchoolList).delete(deleteSchoolList).get(getSchoolById)
-router.route("/school/review/").put(authMiddleware,createSchoolReview);
+router.route("/school/review").put(authMiddleware,createSchoolReview);
 router.route("/school/reviews").get(getschoolReviews)
 module.exports = router;
